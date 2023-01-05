c3de08fddedc586353f4656790e29861af8368f6
102345 <marciliojcg@gmail.com>
2023-01-05 16:48:19 -0300

    Implementação do método BuscarPerfis no arquivo perfisController

--- src/controllers/perfisController.go
+++ src/controllers/perfisController.go
@@ -1,13 +1,36 @@
 package controllers
 
 import (
+	"api/src/banco"
+	"api/src/repositorios"
+	"api/src/respostas"
 	"net/http"
+	"strings"
 )
 
 func CriarPerfil(w http.ResponseWriter, r *http.Request) {
 }
 
 func BuscarPerfis(w http.ResponseWriter, r *http.Request) {
+
+	descricao := strings.ToLower(r.URL.Query().Get("perfil"))
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePerfis(db)
+	perfis, erro := repositorio.Buscar(descricao)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, perfis)
 
 }
 
